5229e8fa2f71266411b3839439b4c86f9c4f780e
BShankar <101615706+Bhimashankarsg@users.noreply.github.com>
2024-11-13 12:55:33 +0530

    Both TC SOP

--- SampleProject_Jenkin/src/test/java/pack2_For_Maven/OrgTestForMavenTest.java
+++ SampleProject_Jenkin/src/test/java/pack2_For_Maven/OrgTestForMavenTest.java
@@ -7,7 +7,15 @@ public class OrgTestForMavenTest {
 @Test
 	public void createOrgTest()
 	{
+	String URL=System.getProperty("url");
+	String BROWSER=System.getProperty("browser","chrome");
+	String USERNAME=System.getProperty("username");
+	String PASSWORD=System.getProperty("password");
 	
+	System.out.println(URL);
+	System.out.println(BROWSER);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
 	
 		System.out.println("Execute createOrgTest");
 	}
